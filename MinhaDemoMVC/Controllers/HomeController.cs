8185551a3db15028a23104ad1495d4dbfd85af4d
Henrique Bittencourt <henriquebs98@outlook.com>
2020-06-29 20:53:15 -0300

    Subindo projeto para repositorio remoto

---
+++ MinhaDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(string id, string categoria)
+        {
+            //var filme = new Filme
+            //{
+            //    Titulo = "Oi",
+            //    DataLancamento = DateTime.Now,
+            //    Genero = null,
+            //    Avaliacao = 10,
+            //    Valor = 2000
+            //};
+            //return RedirectToAction("Privacy", filme);
+            return View();
+        }
+
+        [Route("politica-de-privacidade")]
+        [Route("privacidade")]
+        public IActionResult Privacy(Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
+            return View();
+            //return Json("{'nome':'Henrique'}");
+            //var fileBytes = System.IO.File.ReadAllBytes(@"c:\users\henri\onedrive\documentos\7 - Currículo\CV - Henrique Bittencourt Severo.pdf");
+            //var fileName = "CV - Henrique Bittencourt Severo.pdf";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MinhaDemoMVC/Models/Filme.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [Required(ErrorMessage = "O campo Titulo é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O Título precisa ter entre 3 e 60 caracteres")]
+        public string Titulo { get; set; }
+       
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O campo Data de Lancamento é obrigatório")]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [Required(ErrorMessage = "O campo Genero é obrigatório")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Genero em formato incorreto")]
+        [StringLength(30, ErrorMessage = "Máximo de 30 caracteres")]
+        public string Genero { get; set; }
+        
+        [Range(1,1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "O campo Valor é obrigatório")]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        [Required(ErrorMessage = "O campo Avaliação é obrigatório")]
+        [Display(Name = "Avaliação")]
+        public int Avaliacao { get; set; }
+    }
+}
